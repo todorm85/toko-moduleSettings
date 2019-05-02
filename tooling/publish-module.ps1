d6430c01d53e37ec5c585d6af1a6d12457f37eed
unknown <todorbm85@gmail.com>
2019-05-02 10:53:35 +0300

    Update publish script

--- tooling/publish-module.ps1
+++ tooling/publish-module.ps1
@@ -1,6 +1,1 @@
-Set-Location "$PSScriptRoot\..\"
-
-# git clean -xdf
-
-$apiKey = Read-Host -Prompt "API Key: "
-Publish-Module -Name "toko-posh-dev-tools" -NuGetApiKey $apiKey
+Publish-Module -Name "toko-posh-dev-tools" -NuGetApiKey $Env:NuGetApiKey
